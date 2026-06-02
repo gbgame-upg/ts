cae06266b2e6b92e09c909363bdffb055b28113d
Bùi Huy Tú <buihuytu1001@gmail.com>
2026-06-02 16:50:19 +0700

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using QLHangTonKho.Views;
+
+namespace QLHangTonKho
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Khởi động từ màn hình đăng nhập
+            var login = new LoginWindow();
+            login.Show();
+        }
+    }
+}
